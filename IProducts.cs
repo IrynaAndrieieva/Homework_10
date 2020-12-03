16e189787186106371f8885f8d26638b2088ea21
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 14:55:52 +0200

    Added IProducts

---
+++ IProducts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public class IProducts
+    {
+        public interface Product
+        {
+            string Name { get; }
+
+            double Price { get; }
+
+            double Сonsumption { get; } // расход топлива
+
+            string CreatedBy { get; }
+
+        }
+    }
+}
