6456c971930a1bbad61cc7c2b0924b690cbe05f5
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 14:59:44 +0200

    Added enum TypeOfPassengerCar

---
+++ TypeOfPassengerCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public enum TypeOfPassengerCar
+    {
+        Minivan,
+        SUV, // внедорожник
+        StationWagon, //универсал
+        Hatchback
+    }
+}
