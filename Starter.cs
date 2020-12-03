5e93ad5ee017c91d06bc2f32cf2e655e0c30a11a
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 15:55:24 +0200

    Added Array to metod Run

--- Starter.cs
+++ Starter.cs
@@ -8,5 +8,24 @@ namespace Homework_10
 {
     class Starter
     {
+        private readonly IFullPriceServis fullPriceServis;
+
+        public Starter()
+        {
+            this.fullPriceServis = new FullPriceServis();
+        }
+
+        public void Run()
+        {
+            var fullPrise = this.fullPriceServis.MakeCost();
+            Array.Sort(fullPrise.PassengerCar, new PassengerCarComparer());
+
+            for (int i = 0; i < fullPrise.PassengerCar.Length; i++)
+            {
+                Console.WriteLine($"Name: {fullPrise.PassengerCar[i].Name}");
+            }
+
+            Console.WriteLine($"The cost of cars in the fleet: {fullPrise.Price}");
+        }
     }
 }
