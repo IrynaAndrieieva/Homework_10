ef4093173d742a613636f61a1f6fe66d03ee4264
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 15:08:14 +0200

    Added date to SUVMercedBesenz

---
+++ SUVMercedBesenz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public class SUVMercedBesenz : PassengerCar
+    {
+        public SUVMercedBesenz(double consumption, string createdBy)
+            : base(consumption, createdBy)
+        {
+        }
+
+        public override TypeOfPassengerCar TypeOfPassengerCar => TypeOfPassengerCar.Hatchback;
+
+        public override string Name => "SUV Mercedes-Benz E-Class C238";
+
+        public override double Price => 68000;
+    }
+}
