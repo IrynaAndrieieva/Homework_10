96a8f7a400a87cb95b47a16a734db28dafd09861
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 14:58:47 +0200

    Added PassengerCar

---
+++ PassengerCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public abstract class PassengerCar : Сabstand
+    {
+        protected PassengerCar(double price, string createdBy)
+            : base(price, createdBy)
+        {
+        }
+
+        public abstract TypeOfPassengerCar TypeOfPassengerCar { get; }
+    }
+}
