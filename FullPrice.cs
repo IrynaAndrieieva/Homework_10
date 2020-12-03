7443a3dcdfc901632e1a6f9296510ea2bf871f4c
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 15:38:18 +0200

    Added the ability to calculate the full cost through services

---
+++ FullPrice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public class FullPrice
+    {
+        public PassengerCar[] PassengerCar { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ FullPriceServis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public class FullPriceServis : IFullPriceServis
+    {
+        public FullPriceServis()
+        {
+        }
+
+        public FullPrice MakeCost()
+        {
+            var passengerCar = new PassengerCar[]
+            {
+                new VolkswagenMinivan(12.5,"Volkswagen"),
+                new SUVMercedBesenz(7.0, "Merced Besenz"),
+                new StationWagonAudi(11.5, "Audi"),
+                new HatchbackJaguar (6.5, "Jaguar"),
+            };
+
+            var price = 0d;
+            for (var i = 0; i < passengerCar.Length; i++)
+            {
+                price += passengerCar[i].Price;
+            }
+
+            return new FullPrice { PassengerCar = passengerCar, Price = price };
+        }
+
+    }
+}
