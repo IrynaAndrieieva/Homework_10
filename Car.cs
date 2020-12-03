c6a8ba66d0d5b74256d2f3a2cb52b10543e59b46
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 14:57:55 +0200

    Make Car

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+    public abstract class Car : Сabstand
+    {
+        protected Car(double consumption, string createdBy)
+            : base(consumption, createdBy)
+        {
+        }
+
+        public abstract bool IsElectricCar { get; }
+    }
+}
