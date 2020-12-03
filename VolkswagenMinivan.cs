7427e40e80d7f8d360f9b352cd88bb1eced0c5b3
Iryna <ortofosfatnyi@gmail.com>
2020-12-03 15:09:49 +0200

    Added date to VolkswagenMinivan

---
+++ VolkswagenMinivan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10
+{
+     public class VolkswagenMinivan : PassengerCar
+    {
+        public VolkswagenMinivan(double consumption, string createdBy)
+            : base(consumption, createdBy)
+        {
+        }
+
+        public override TypeOfPassengerCar TypeOfPassengerCar => TypeOfPassengerCar.Minivan;
+
+        public override string Name => "Multivan Volkswagen T6.1";
+
+        public override double Price => 57000;
+    }
+}
